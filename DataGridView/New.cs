b0a122d930a7b6d29bcd1e16bdb787a109148c42
Murlocza3 <nakls6272001@gmail.com>
2020-06-28 17:47:52 +0300

    XML

---
+++ DataGridView/New.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DataGridView
+{
+    public partial class New : Form
+    {
+        public New()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
